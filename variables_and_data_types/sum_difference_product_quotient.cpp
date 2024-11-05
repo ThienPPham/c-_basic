286dde8a0b6eb42ab7cd6ba171767b3cd4d48a87
ThienPPham <thienversiopp129.it.vn@gmail.com>
2024-11-05 11:58:49 +0700

    declare variables and calculate sum, difference, product, quotient

---
+++ variables_and_data_types/sum_difference_product_quotient.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a = 6;
+	int b = 2;
+	
+	cout << "a + b = " << a + b << endl 
+		<< "a - b = " << a - b << endl
+		<< "a * b = " << a * b << endl
+		<< "a / b = " << a / b;
+	return 0; 	
+}
