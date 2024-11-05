23049eee28e56121acb3527df073488f136f3d47
ThienPPham <thienversiopp129.it.vn@gmail.com>
2024-11-05 11:52:17 +0700

    variables and how to declare variables in c++, calculating the diference betwween two numbers

---
+++ variables_and_data_types/declare_variables_in_C++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	//int a = 254;
+	//int b = 343;
+	int a = 8343;
+	int b = 6453;
+	
+	//cout << "a + b = " << a + b;
+	cout << "a - b = " << a - b;
+	return 0;
+}
