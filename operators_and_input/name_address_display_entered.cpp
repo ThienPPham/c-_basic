759e124e2a669698a2f81787a9be8f3295be1eb9
ThienPPham <thienversiopp129.it.vn@gmail.com>
2024-11-06 15:35:54 +0700

    enter name and address then display the information just entered

---
+++ operators_and_input/name_address_display_entered.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	string name;
+	string address;
+	cin >> name;
+	cin >> address;
+	cout << "Name: " << name << endl << "Address: " << address;
+	return 0;
+}
