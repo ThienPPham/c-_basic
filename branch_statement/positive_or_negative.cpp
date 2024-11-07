dcb83d5b7a678c55ff022a6ae00086b4ffa76984
ThienPPham <thienversiopp129.it.vn@gmail.com>
2024-11-07 11:30:21 +0700

    check if a number is negative or positive

---
+++ branch_statement/positive_or_negative.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	if(n > 0){
+		cout << "n is a positive number";
+	}if(n < 0){
+		cout << "n is a negative number";
+	}if(n == 0){
+		cout << "n is equal to 0";
+	}
+	return 0;
+}
